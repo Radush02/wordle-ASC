0c296862487c8c04cdcde567813febf47f95f756
Radu Cordunianu <82465411+Radush02@users.noreply.github.com>
2022-11-02 19:56:03 +0200

    Update main.py
    ap. fiecarei lit. pt. calc. entrop.

--- main.py
+++ main.py
@@ -1,27 +1,50 @@
 from urllib.request import urlopen
 import random
-data=urlopen("https://cs.unibuc.ro/~crusu/asc/cuvinte_wordle.txt")
+from multiprocessing import Process
 
+
+data=urlopen("https://cs.unibuc.ro/~crusu/asc/cuvinte_wordle.txt")
 cuvinte=[]
 for line in data:
     cuvinte.append(str(line)[2:7])
-
 cuvinte=cuvinte[:len(cuvinte)-1]
 deGhicit=cuvinte[random.randint(0,len(cuvinte)-1)]
 
-#print(deGhicit)
-x=input("Cuvant: ")
-while x != deGhicit:
-    rez=""
-    for i in range(0,5):
-        if x[i]==deGhicit[i]:
-            rez+="2"
-        elif deGhicit.find(x[i]) != -1:
-            rez+="1"
+apr=[]
+for i in range(5):
+    apr.append(dict([(chr(i), 0) for i in range(65, 91)]))
+for c in cuvinte:
+    for i in range(5):
+        apr[i][c[i]]+=1
+
+def jocWordle(x):
+    try:
+        aux=deGhicit
+        while x != deGhicit:
+            rez = ""
+            for i in range(0, 5):
+                if x[i] == deGhicit[i]:
+                    rez += "2"
+                else:
+                    rez += "0"
+            print(str(rez))
+            x = str(input("Cuvant= "))
+            x = x.upper()
         else:
-            rez+="0"
-    print(str(rez))
-    #print(deGhicit)
-    x=input("Cuvant: ")
-else:
-    print(f"Cuvantul era {deGhicit}")
+            print(f"Cuvantul era {deGhicit}")
+    except IndexError:
+        x=x[:4]
+        jocWordle(x)
+
+def main():
+    # x=str(input("Cuvant= "))
+    # x=x.upper()
+    # jocWordle(x)
+    for i in range(5):
+        apr[i]=dict(sorted(apr[i].items(),reverse=True, key=lambda item: item[1]))
+        print(apr[i])
+
+if __name__=="__main__":
+    main()
+
+
