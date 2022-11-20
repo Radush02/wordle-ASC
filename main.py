41c6f7b153996774fddbb0964559df4c511228b9
Radu Cordunianu <82465411+Radush02@users.noreply.github.com>
2022-11-20 22:39:50 +0200

    Adaugat comunicare intre fisiere

--- main.py
+++ main.py
@@ -24,39 +24,43 @@ def init_cuvinte():
 
 cnt_jocuri=0
 lg_cuv=len(cuvinte)
-def jocWordle(incercare):
-    try:
-        global cuvinte,meciuriJucate,meciuriTotale
-        while incercare != deGhicit:
-            meciuriJucate += 1
-            rez = ""
-            for i in  range(5):
-                if incercare[i] == deGhicit[i]:
-                    rez += "2"
-                elif incercare[i] in deGhicit:
-                    rez += "1"
-                else:
-                    rez += "0"
-            #print(str(rez))
-            for i in range(5):
-                if rez[i]=="0":
-                    cuvinte=([cuv for cuv in cuvinte if incercare[i] not in cuv])
-                elif rez[i]=="2":
-                    cuvinte=([cuv for cuv in cuvinte if incercare[i] == cuv[i]])
-                elif rez[i]=="1":
-                    cuvinte=([cuv for cuv in cuvinte if incercare[i] in cuv])
-            try:
-                cuvinte.remove(incercare)
-            except:
-                pass
-            incercare=getEntropie()
+fin=open("fin.txt","w")
+fout=open("fout.txt","w")
+def jocWordle(incercare,fisier):
+    global cuvinte,meciuriJucate,meciuriTotale
+    meciuriJucate += 1
+    rez = ""
+    for i in range(5):
+        if incercare[i] == deGhicit[i]:
+            rez += "2"
+        elif incercare[i] in deGhicit:
+            rez += "1"
         else:
-            print(f"Cuvantul era {deGhicit}, ghicit in {meciuriJucate} incercari")
-            meciuriTotale+=meciuriJucate
-    except IndexError:
-        incercare=incercare[:4]
-        jocWordle(incercare)
+            rez += "0"
+    print(rez,file=fout)
+    parseRezultat(rez,incercare,fisier)
 
+def parseRezultat(rez,incercare,fisier):
+    global cuvinte
+    for i in range(5):
+        if rez[i] == "0":
+            cuvinte = ([cuv for cuv in cuvinte if incercare[i] not in cuv])
+        elif rez[i] == "2":
+            cuvinte = ([cuv for cuv in cuvinte if incercare[i] == cuv[i]])
+        elif rez[i] == "1":
+            cuvinte = ([cuv for cuv in cuvinte if incercare[i] in cuv])
+    try:
+        cuvinte.remove(incercare)
+    except:
+        pass
+    incercare = getEntropie()
+    print(incercare, file=fisier)
+    print(incercare, file=fin)
+    if incercare is deGhicit:
+         print(f"Cuvantul era {deGhicit}, ghicit in {meciuriJucate} incercari")
+         exit()
+    else:
+         jocWordle(incercare,fisier)
 meciuriJucate=0
 meciuriTotale=0
 back={}
@@ -78,47 +82,41 @@ def genlist(n,a,i):
     genlist(n,a,i+1)
 genlist(5,a,0)
 
-
 def getEntropie():
-    if os.path.exists("entropie.txt"):
-        os.remove("entropie.txt")
     entropie = {}
-    with open("entropie.txt","a") as f:
-        for x in cuvinte:
-            for i in back:
-                back[i]=0
-            for deGhicit in cuvinte:
-                rez=""
-                for i in range(5):
-                            if x[i] == deGhicit[i]:
-                                rez += "2"
-                            elif x[i] in deGhicit:
-                                rez += "1"
-                            else:
-                                rez += "0"
-                back[rez]+=1
-            ent=0
-            for i in back:
-                p_i=back[i]/lg_cuv
-                try:
-                    ent+=p_i*math.log2(1/p_i)
-                except ZeroDivisionError:
-                    ent+=0
-            print(x,ent,file=f)
-            entropie[x]=ent
+    for x in cuvinte:
+        for i in back:
+            back[i]=0
+        for deGhicit in cuvinte:
+            rez=""
+            for i in range(5):
+                        if x[i] == deGhicit[i]:
+                            rez += "2"
+                        elif x[i] in deGhicit:
+                            rez += "1"
+                        else:
+                            rez += "0"
+            back[rez]+=1
+        ent=0
+        for i in back:
+            p_i=back[i]/lg_cuv
+            try:
+                ent+=p_i*math.log2(1/p_i)
+            except ZeroDivisionError:
+                ent+=0
+        entropie[x]=ent
     return max(entropie,key=entropie.get)
 
 
 cuv_optim="TAREI"
 def main():
-    # x=str(input("Cuvant= "))
-    # x=x.upper()
     global meciuriJucate
-    x=int(input("Numar jocuri: "))
-    for i in range(x):
-        init_cuvinte()
-        jocWordle(cuv_optim)
-        meciuriJucate=0
-    print("Medie jocuri: ",meciuriTotale/x)
+    if os.path.exists("solutii.txt"):
+        os.remove("solutii.txt")
+    f = open("solutii.txt", "a")
+    print(cuv_optim,file=f)
+    init_cuvinte()
+    jocWordle(cuv_optim,f)
+
 if __name__=="__main__":
     main()
